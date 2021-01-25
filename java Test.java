2e23f81e48142a5c65ad2335efa361c80cb1e5dc
TRAN TRUNG KIEN <75594668+ttk99@users.noreply.github.com>
2021-01-25 11:22:31 +0900

    Add files via upload

---
+++ java Test.java
@@ -0,0 +1,28 @@
+
+
+ pwd
+ cd
+ cd Desktop/
+ pwd
+ ls
+ cd JavaFX
+ pwd
+ ls
+ cat Test8.java
+ javac Test.java
+ cat ~/.bash_profile
+ javac -version
+ java -version
+ touch .bash_profile
+ ls .bash*
+ ls
+ ls -a
+ cd
+ cd desktop
+ cd JavaFX
+ ls
+ alias javac='/c/pleiades/java/8/bin/javac.exe'
+ alias java='/c/pleiades/java/8/bin/java.exe'
+ javac Test8.java
+ java Test8
+
