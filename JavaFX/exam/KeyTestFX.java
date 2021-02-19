7bc4329eef45889c76905d3788d5e21b057ee4b7
TRAN TRUNG KIEN <75594668+ttk99@users.noreply.github.com>
2021-02-19 12:38:35 +0900

    Add files via upload

---
+++ JavaFX/exam/KeyTestFX.java
@@ -0,0 +1,93 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.animation.AnimationTimer;
+import javafx.scene.input.KeyEvent;
+import javafx.event.EventHandler;
+
+public class KeyTestFX extends Application{
+	private Breakoutthread breakoutthread;
+
+	public static void main (String [] args){
+		launch( args);
+	}
+	@Override
+	public void start (Stage stage ){
+
+		Key key = new Key();
+		stage.setTitle("Ball Test");
+
+		Pane pane = new Pane();
+		Scene scene = new Scene (pane);
+		stage.setScene ( scene );
+
+		scene.setOnKeyPressed(
+			new EventHandler<KeyEvent>(){
+				public void handle (KeyEvent e){
+					key.keyPressed( e );
+			}
+			}
+			);
+
+		Canvas canvas = new Canvas(640, 480 );
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		pane.getChildren().add( canvas );
+
+		breakoutthread = new Breakoutthread( gc );
+		breakoutthread.start();
+
+		stage.show();
+	}
+}
+
+class Breakoutthread extends AnimationTimer {
+
+	private GraphicsContext gc;
+	private Ball ball;
+
+	public Breakoutthread( GraphicsContext gc ){
+		this.gc = gc;
+		ball = new Ball();
+	}
+	@Override
+	public void handle (long time ){
+		gc.clearRect (0, 0, 640,480);
+
+		ball.draw(gc);
+		ball.move();
+	}
+}
+class Ball{
+
+	private int x;
+	private int y;
+	private int x_speed;
+	private int y_speed;
+
+	public Ball(){
+		this.x = 20;
+		this.y = 20;
+		this.x_speed = 5;
+		this.y_speed = 5;
+	}
+	public void draw ( GraphicsContext gc ){
+		gc.setFill (Color.RED );
+		gc.fillOval (x, y, 20, 20);
+	}
+
+	public void move(){
+		this.x += this.x_speed;
+		this.y += this.y_speed;
+	}
+}
+
+class Key{
+
+   public void keyPressed( KeyEvent e ) {
+   	System.out.println( e.getCode() + "pressed.");
+   }
+}
